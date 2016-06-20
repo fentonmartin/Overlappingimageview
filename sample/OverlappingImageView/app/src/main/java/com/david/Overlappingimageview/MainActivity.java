4f681eae81354c7bbfb8e7a410a09608d5885b02
david <david@chatway.com>
2016-06-20 08:43:12 +0300

    first  code commit

---
+++ sample/OverlappingImageView/app/src/main/java/com/david/Overlappingimageview/MainActivity.java
@@ -0,0 +1,93 @@
+package com.david.Overlappingimageview;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final static String GITHUB_URL = "https://github.com/davidHarush/Overlappingimageview";
+
+    private FloatingActionButton mFab;
+    private OverlappingImageView mImg2, mImg3, mImg4;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mFab = (FloatingActionButton) findViewById(R.id.fab);
+
+        mImg2 = ((OverlappingImageView) findViewById(R.id.imageView1))
+                .setMaskShape(android.R.drawable.picture_frame)
+                .setDrawOnFront(false);
+
+        mImg3 = ((OverlappingImageView) findViewById(R.id.imageView2))
+                .setMaskShape(R.drawable.mask_shape);
+
+        mImg4 = ((OverlappingImageView) findViewById(R.id.imageView3))
+                .setMaskShape(R.drawable.mask_shape)
+                .setDrawOnFront(false);
+
+
+        mFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mImg2.toggleMask();
+                mImg3.toggleMask();
+                mImg4.toggleMask();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_info) {
+            showInfoDialog();
+            return true;
+        }
+
+        if (id == R.id.action_github) {
+            openGitHub();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void showInfoDialog() {
+
+        AlertDialog dialog = new AlertDialog.Builder(this).
+                setTitle("INFO").
+                setMessage(Html.fromHtml("this is example for a simple ImageView with adding " +
+                        "Functionality to Draw Drawable on the front\\background of the imageView.<br/>" +
+                        "<a href=" + GITHUB_URL + "> for more Info go to GitHub </a>")).
+                setPositiveButton("ok", null).
+                show();
+        ((TextView) dialog.findViewById(android.R.id.message)).setMovementMethod(LinkMovementMethod.getInstance());
+    }
+
+    private void openGitHub() {
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(GITHUB_URL));
+        startActivity(browserIntent);
+    }
+
+}
---
+++ sample/OverlappingImageView/app/src/main/java/com/david/Overlappingimageview/OverlappingImageView.java
@@ -0,0 +1,151 @@
+package com.david.Overlappingimageview;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.widget.ImageView;
+
+/**
+ * Created by David Harush
+ * on 22/03/2016.
+ * <br/>
+ * <br/>
+ * ImageView with options to Draw Drawable on the front or the back of the image.
+ * <br/>
+ * You can supply the Drawable as xml shape resources or png image resources.
+ * <br/>
+ * The Drawable will resize according to the ImageView size.
+ */
+public class OverlappingImageView extends ImageView {
+
+    public static final String TAG = OverlappingImageView.class.getSimpleName();
+
+    private boolean mMaskEnable = false;
+    private boolean mDrawOnFront = true;
+    private int mShapeID = 0;
+    private String mAndroidId;
+
+    // ================================== //
+    // ========== CONSTRUCTORS ========== //
+    // ================================== //
+
+    public OverlappingImageView(Context context) {
+        super(context);
+        if (!isInEditMode()) {
+            init();
+        }
+    }
+
+    public OverlappingImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        if (!isInEditMode()) {
+            init();
+        }
+    }
+
+    public OverlappingImageView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        if (!isInEditMode()) {
+            init();
+        }
+    }
+
+    // ======================================== //
+    // ========== OVERRIDDEN METHODS ========== //
+    // ======================================== //
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        if (mDrawOnFront) { // Draw the mask on Front
+            super.onDraw(canvas);
+            drawMaskImage(canvas);
+        } else { // Draw the mask on Background
+            drawMaskImage(canvas);
+            super.onDraw(canvas);
+        }
+    }
+
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        toggleMask();
+        return super.onTouchEvent(event);
+    }
+
+    // ================================ //
+    // ==== PRIVATE CLASS METHODS ===== //
+    // ================================ //
+
+    private void init() {
+        mAndroidId = getResources().getResourceEntryName(getId());
+    }
+
+    private void drawMaskImage(Canvas canvas) {
+        if (mMaskEnable && mShapeID != 0) {
+            Drawable drawable;
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP_MR1) {
+                drawable = getResources().getDrawable(mShapeID, getContext().getTheme());
+            } else {
+                drawable = getResources().getDrawable(mShapeID);
+            }
+
+            if (drawable != null) {
+                drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+                drawable.draw(canvas);
+            }
+        } else if (mShapeID == 0) {
+            Log.w(TAG, "WARNING ─>> Mask not set for " + TAG + ": android:id=\"@+id/" + mAndroidId + "\" !!!");
+        }
+
+    }
+
+    // ================================ //
+    // ==== PUBLIC CLASS METHODS ====== //
+    // ================================ //
+
+    public void ShowMask(boolean mask) {
+        if (mask == mMaskEnable) {
+            return;
+        }
+        this.mMaskEnable = mask;
+        invalidate();
+    }
+
+    public void toggleMask() {
+        ShowMask(!mMaskEnable);
+    }
+
+    /**
+     * if onFront == true -> Draw mask On front of the image
+     * <br/>
+     * if onBack == false -> Draw mask On back of the image
+     */
+    public OverlappingImageView setDrawOnFront(boolean onFront) {
+        mDrawOnFront = onFront;
+        return this;
+    }
+
+    /**
+     * Set drawable res to draw on the imageView
+     *
+     * @param id of the drawable.
+     */
+    public OverlappingImageView setMaskShape(int id) {
+        mShapeID = id;
+        return this;
+    }
+
+    /**
+     * Return state of the mask
+     */
+    public boolean isMaskEenable() {
+        return this.mMaskEnable;
+    }
+
+
+}
